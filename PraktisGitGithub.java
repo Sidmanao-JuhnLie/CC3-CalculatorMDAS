3ea436640cb0ad2e3f83112dba849ce4dd9bc8d2
Juhn Lie <sidmanaojuhnlie30@gmail.com>
2026-01-22 20:42:31 +0800

    Add files via upload

---
+++ PraktisGitGithub.java
@@ -0,0 +1,45 @@
+package practice;
+import java.util.Scanner;
+public class PraktisGitGithub {
+    static int operators(int a, int b, char isOperators) {
+        if (isOperators == '+') {
+            return a + b;
+        } else if (isOperators == '-') {
+            return a - b;
+        } else if (isOperators == '*') {
+            return a * b;
+        } else if (isOperators == '/') {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot Divide by Zero");
+            } else {
+                return a / b;
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid Operator");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter Operator: ");
+        char isOperators = scanner.next().charAt(0);
+
+        System.out.print("Enter First Number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int num2 = scanner.nextInt();
+
+        try {
+            int result = operators(num1, num2, isOperators);
+            System.out.println("Answer: " + result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
